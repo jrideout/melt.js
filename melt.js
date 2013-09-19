594feceba66149772d2c223de8bc66b0a6778eaf
Jacob Rideout <jacob@jacobrideout.net>
2013-09-18 21:34:19 -0400

    add cast

--- melt.js
+++ melt.js
@@ -1,42 +1,95 @@
-function melt(data, keep, varName, valName, noAddId) {
+var melt = (function (undefined) {
+
+  function set(keys) {
+    var s = {}, keys = keys || [];
+    for (var i = 0; i < keys.length; i++) {
+      s[keys[i]] = 1;
+    }
+    return s;
+  }
+
+  function extend(obj, copy) {
+    for (var k in copy) {
+      obj[k] = copy[k];
+    }
+    return obj;
+  }
+
+  function melt(data, keep, varName, valName, noAddId) {
     var newData = [],
-        varName = varName || 'variable',
-        valName = valName || 'value',
-        keepSet = set(keep);
+      varName = varName || 'variable',
+      valName = valName || 'value',
+      keepSet = set(keep);
 
-    function set(keys) {
-        var s = {}, keys = keys || [];
-        for (var i = 0; i < keys.length; i++) {
-            s[keys[i]] = 1;
+    data.forEach(function (row, rowId) {
+      var save = noAddId ? {} : {_id: rowID},
+        vars = [],
+        vals = [];
+      for (var k in row) {
+        if (keepSet[k]) {
+          save[k] = row[k];
+        } else {
+          vars.push(k);
+          vals.push(row[k]);
         }
-        return s;
-    }
-
-    function extend(obj, copy) {
-        for (k in copy) {
-            obj[k] = copy[k];
-        }
-        return obj;
-    }
-
-    data.forEach(function(row, i) {
-        var save = noAddId ? {} : {_id: i},
-            vars = [],
-            vals = [];
-        for (k in row) {
-            if (keepSet[k]) {
-                save[k] = row[k];
-            } else {
-                vars.push(k);
-                vals.push(row[k]);
-            }
-        }
-        for (var i = 0; i < vars.length; i++) {
-            var newRow = {};
-            newRow[varName] = vars[i];
-            newRow[valName] = vals[i];
-            newData.push(extend(newRow, save));
-        }
+      }
+      for (var i = 0; i < vars.length; i++) {
+        var newRow = {};
+        newRow[varName] = vars[i];
+        newRow[valName] = vals[i];
+        newData.push(extend(newRow, save));
+      }
     });
     return newData;
-}
+  }
+
+  function cast(data, keep, fun) {
+    var funArgs = Array.prototype.slice.call(arguments, 3),
+    keepData = {},
+    funStash = {};
+
+    data.forEach(function (row) {
+      var newRow = {};
+      var aggKey = keep.map(function (key) {
+        return newRow[key] = row[key];
+      }).join('\0');
+
+      if (!keepData[aggKey])
+        keepData[aggKey] = extend({}, newRow);
+      var args = funArgs.slice(0);
+      args.unshift(row, keepData[aggKey]);
+      keepData[aggKey] = fun.apply(funStash, args);
+    });
+
+    return Object.keys(keepData).map(function (k) {
+      return keepData[k];
+    });
+  }
+
+  cast.sum = function (row, acc, name, cols) {
+    if (!this.sum) {
+      var sum = function (row, acc, name, cols) {
+        acc[name] = cols.reduce(function (a, k) {
+          return a + row[k];
+        }, acc[name] || 0);
+        return acc;
+      };
+      this.sum = function (row, acc) {
+        if (cols === undefined) cols = name;
+        if (typeof (cols) == 'string') cols = [cols];
+        return sum(row, acc, name, cols);
+      };
+    }
+    return this.sum(row, acc);
+  };
+
+  cast.count = function (row, acc, name) {
+    if (!acc[name]) acc[name] = 0;
+    acc[name] += 1;
+    return acc;
+  };
+
+  melt.cast = cast;
+  return melt;
+})(undefined),
+  cast = melt.cast;
